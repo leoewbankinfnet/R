595e6aeacca8add7b057509647f056525b041a66
leoewbankinfnet <leonardo.ewbank@al.infnet.edu.br>
2024-03-03 16:10:03 -0300

    segundo

---
+++ R_Proj.R
@@ -0,0 +1,7 @@
+#gitignore -> ignora os arquivos/pastas mencionadas
+library(renv)
+library(tidyverse)
+library(usethis)
+renv::init() ##cria uma pasta para armazenar os pacotes que forem inicializados
+renv::status() ##verifica o versionamento daquilo que está guardado e se há algo diferente não armazenado/registrado
+renv::snapshot() ##registrar os pacotes novos instalados no ambiente
