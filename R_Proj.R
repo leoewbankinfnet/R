3d704b61f504488b4a6debcc8707924f9512ad49
leoewbankinfnet <leonardo.ewbank@al.infnet.edu.br>
2024-03-05 20:05:48 -0300

    novas linnhas

--- R_Proj.R
+++ R_Proj.R
@@ -1,7 +1,10 @@
 #gitignore -> ignora os arquivos/pastas mencionadas
 library(renv)
 library(tidyverse)
-library(usethis)
+library(knitr)
+library(data.table)
+library(rvest)
 renv::init() ##cria uma pasta para armazenar os pacotes que forem inicializados
 renv::status() ##verifica o versionamento daquilo que está guardado e se há algo diferente não armazenado/registrado
 renv::snapshot() ##registrar os pacotes novos instalados no ambiente
+
